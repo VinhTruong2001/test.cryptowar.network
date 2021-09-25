94de5c7e41cb10ec5fc359e15160d82a92474f5e
Tam Nguyen <nntam2013@gmail.com>
2021-09-25 21:15:42 +0700

    chore: remove staking type

--- frontend/src/interfaces/State.ts
+++ frontend/src/interfaces/State.ts
@@ -5,8 +5,8 @@ import { Contracts } from './Contracts';
 import { Nft } from './Nft';
 import { IShield } from './Shield';
 
-export type StakeType = 'skill' | 'skill2' | 'lp' | 'lp2';
-export const allStakeTypes: StakeType[] = ['skill', 'skill2', 'lp'];
+export type StakeType = 'lp' | 'lp2';
+export const allStakeTypes: StakeType[] = ['lp'];
 
 export function isStakeType(stakeType: string): stakeType is StakeType {
   return allStakeTypes.includes(stakeType as StakeType);
