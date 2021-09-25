c5e7095bba7521272a19aa5566325957108d4738
Tam Nguyen Ngoc <34363701+nntam2013@users.noreply.github.com>
2021-09-25 22:09:21 +0700

    chore: clean up (#29)

--- frontend/src/stake-types.ts
+++ frontend/src/stake-types.ts
@@ -13,19 +13,8 @@ export interface StakingContractEntry {
   stakingTokenAddress: string;
 }
 
-export const stakingContractsInfo: Partial<Record<StakeType, Partial<StakingContractEntry>>> = {
-  skill: {
-    stakingRewardsAddress: process.env.VUE_APP_SKILL_STAKING_REWARDS_CONTRACT_ADDRESS,
-    stakingTokenAddress: process.env.VUE_APP_XBLADE_TOKEN_CONTRACT_ADDRESS
-  }
-};
+export const stakingContractsInfo: Partial<Record<StakeType, Partial<StakingContractEntry>>> = {};
 
-if(availableStakingContracts.includes('skill2')) {
-  stakingContractsInfo.skill2 = {
-    stakingRewardsAddress: process.env.VUE_APP_SKILL2_STAKING_REWARDS_CONTRACT_ADDRESS,
-    stakingTokenAddress: process.env.VUE_APP_SKILL2_TOKEN_CONTRACT_ADDRESS
-  };
-}
 
 if(availableStakingContracts.includes('lp')) {
   stakingContractsInfo.lp = {
@@ -49,17 +38,6 @@ interface HumanReadableDetailsForStakeType {
 }
 
 const defaultHumanReadableDetailsForStakeTypes: Record<StakeType, HumanReadableDetailsForStakeType> = {
-  skill: {
-    stakeTokenName: 'xBlade',
-    rewardTokenName: 'xBlade',
-    stakeTitle: 'xBlade for xBlade (Old)',
-    deprecated: true
-  },
-  skill2: {
-    stakeTokenName: 'xBlade',
-    rewardTokenName: 'xBlade',
-    stakeTitle: 'xBlade for xBlade'
-  },
   lp: {
     stakeTokenName: 'xBlade-WBNB',
     rewardTokenName: 'xBlade',
@@ -79,4 +57,4 @@ const stakeTypeForUnclaimedRewards = process.env.VUE_APP_STAKE_TYPE_FOR_UNCLAIME
 export const stakeTypeThatCanHaveUnclaimedRewardsStakedTo: StakeType =
   stakeTypeForUnclaimedRewards && isStakeType(stakeTypeForUnclaimedRewards)
     ? stakeTypeForUnclaimedRewards
-    : 'skill';
+    : 'lp';
--- migrations/5_bscmainnet_deploy_upgradable_staking.js
+++ migrations/5_bscmainnet_deploy_upgradable_staking.js
@@ -1,17 +1,17 @@
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
-const LP2StakingRewardsUpgradeable = artifacts.require("LP2StakingRewardsUpgradeable");
+const LPStakingRewardsUpgradeable = artifacts.require("LPStakingRewardsUpgradeable");
 
 module.exports = async function (deployer, network, accounts) {
-  // Comment for ignore LP2 staking
   if (network === 'bscmainnet' || network === 'bscmainnet-fork') {
     const ownerAddress = accounts[0];
     const rewardDistributorAddress = '0xC2573A26297a0c952C92bb48Fdcb6929524F7F48';
 
     const xBladeTokenAddress = '0x154A9F9cbd3449AD22FDaE23044319D6eF2a1Fab';
     const lpTokenAddress = '0xC19dfd34D3ba5816dF9CBDaa02D32A9F8dc6F6fC';
+    const stakingDuration = 0;
 
-    await deployProxy(LP2StakingRewardsUpgradeable, [ownerAddress, rewardDistributorAddress, xBladeTokenAddress, lpTokenAddress, 0], { deployer });
+    await deployProxy(LPStakingRewardsUpgradeable, [ownerAddress, rewardDistributorAddress, xBladeTokenAddress, lpTokenAddress, stakingDuration], { deployer });
   }
 };
 
