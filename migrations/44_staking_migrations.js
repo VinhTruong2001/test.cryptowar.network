f300b6fdbb45767a73f8968c3a47b06c86fdc9dc
Adsigma <hong@cloudjetkpi.com>
2021-10-27 23:46:00 +0700

    deploy

--- migrations/44_staking_migrations.js
+++ migrations/44_staking_migrations.js
@@ -4,19 +4,19 @@ const BNBLPStakingRewardsUpgradeable = artifacts.require(
   "BNBLPStakingRewardsUpgradeable"
 );
 
-const XBNLPStakingRewardsUpgradeable = artifacts.require(
-  "XBNLPStakingRewardsUpgradeable"
-);
+// const XBNLPStakingRewardsUpgradeable = artifacts.require(
+//   "XBNLPStakingRewardsUpgradeable"
+// );
 
-const BUSDLPStakingRewardsUpgradeable = artifacts.require(
-  "BUSDLPStakingRewardsUpgradeable"
-);
+// const BUSDLPStakingRewardsUpgradeable = artifacts.require(
+//   "BUSDLPStakingRewardsUpgradeable"
+// );
 
 module.exports = async function (deployer, network, accounts) {
   if (network === "bscmainnet" || network === "bscmainnet-fork") {
     const ownerAddress = accounts[0];
     const rewardDistributorAddress =
-      "0xC2573A26297a0c952C92bb48Fdcb6929524F7F48";
+      "0x5678917FfEb77827Aafc33419E99DaCd707313a9";
 
     const xBladeTokenAddress = "0x27a339d9B59b21390d7209b78a839868E319301B"; // Reward token
     const bnbLPTokenAddress = "0xfea1510bd3ea668a4d14f7f1d95f7ab4ecaa66ed"; // BNB-xBlade Staking token
