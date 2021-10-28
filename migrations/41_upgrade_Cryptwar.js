5be49f7a9f9ab1a0558b7ef4d6758a6254b1948a
Tam Nguyen <nntam2013@gmail.com>
2021-10-28 09:24:03 +0700

    fix: typo

--- migrations/41_upgrade_Cryptwar.js
+++ migrations/41_upgrade_Cryptwar.js
@@ -6,14 +6,14 @@ let PancakeUtil = artifacts.require("PancakeUtil");
 
 module.exports = async function (deployer, network, accounts) {
 
-  // await deployer.deploy(PancakeUtil);
-  // PancakeUtil = await PancakeUtil.at("0x8803B34E863FA0B2f6e840c38024A9D2252EfE18");
-  const pancake = await PancakeUtil.deployed();
-  // await deployer.link(pancake, Cryptowars);
-  
-  // const proxyAddress = "0x8781413C768f207699D51f42b909c5d6A9D9aD36"; testnet
+  await deployer.deploy(PancakeUtil);
+
+  await deployer.link(PancakeUtil, CryptoWars);
+
+  // const proxyAddress = "0x8781413C768f207699D51f42b909c5d6A9D9aD36"; //testnet
   const proxyAddress = "0x8BA9f0841cFA75d7e2c7a316b048b04c98C95cA4"; //mainnetd
-  let cw = await upgradeProxy(proxyAddress, Cryptowars, {
+  const game = await CryptoWars.deployed()
+  await upgradeProxy(proxyAddress, game, {
     deployer,
     unsafeAllow: ["external-library-linking"],
   });
