49ae34d755f0a1082f165dcd3d9722ab1bc423a2
James <james@Jamess-MacBook-Pro.local>
2021-11-02 01:54:39 +0700

    utils

--- migrations/41_upgrade_Cryptwar.js
+++ migrations/41_upgrade_Cryptwar.js
@@ -6,11 +6,11 @@ let PancakeUtil = artifacts.require("PancakeUtil");
 
 module.exports = async function (deployer, network, accounts) {
 
-  await deployer.deploy(PancakeUtil);
+  // await deployer.deploy(PancakeUtil);
 
-  await deployer.link(PancakeUtil, CryptoWars);
+  // await deployer.link(PancakeUtil, CryptoWars);
 
-  // await CryptoWars.link('PancakeUtil','0x2E4Bb0A60734aFD6A7a03dacE82247c1531f9ae5');
+  await CryptoWars.link('PancakeUtil','0x1a354d146E1f6aB8a68C1e3c0ac48d36d47A1965');
 
   // const proxyAddress = "0x8781413C768f207699D51f42b909c5d6A9D9aD36"; //testnet
   // const proxyAddress = "0x8BA9f0841cFA75d7e2c7a316b048b04c98C95cA4"; //mainnetd
