21b817b71470ed97b1969f55ff6dacbbfbca25e3
Tam Nguyen <nntam2013@gmail.com>
2021-11-06 15:46:54 +0700

    migration

--- migrations/41_upgrade_Cryptwar.js
+++ migrations/41_upgrade_Cryptwar.js
@@ -6,14 +6,14 @@ let PancakeUtil = artifacts.require("PancakeUtil");
 
 module.exports = async function (deployer, network, accounts) {
 
-  // await deployer.deploy(PancakeUtil);
+  await deployer.deploy(PancakeUtil);
 
-  // await deployer.link(PancakeUtil, CryptoWars);
+  await deployer.link(PancakeUtil, CryptoWars);
 
-  await CryptoWars.link('PancakeUtil','0xC2AABB5820325D2C229fb085E9f34CA366108d51');
+  await CryptoWars.link('PancakeUtil','0xA51c29c2d39F8D0e5c689463dE6e03c9096B6cE2');
 
   // const proxyAddress = "0x8781413C768f207699D51f42b909c5d6A9D9aD36"; //testnet
-  
+
   // const game = await CryptoWars.at('0x8BA9f0841cFA75d7e2c7a316b048b04c98C95cA4');
   const proxyAddress = "0x8BA9f0841cFA75d7e2c7a316b048b04c98C95cA4"; //mainnet
   await upgradeProxy(proxyAddress, CryptoWars, {
@@ -22,8 +22,8 @@ module.exports = async function (deployer, network, accounts) {
   });
 
   // const cwControllerAddress = "0xAadfa537ecA54d3d7655C4117bBFB83B9bF6035a"; // controller address
-  // const game = await CryptoWars.at('0x8BA9f0841cFA75d7e2c7a316b048b04c98C95cA4');
-  // await game.setCWController(cwControllerAddress);
+  const game = await CryptoWars.at('0x8BA9f0841cFA75d7e2c7a316b048b04c98C95cA4');
+  await game.setSupportFeeRate(10);
 
 
 };
