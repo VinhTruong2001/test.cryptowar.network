92fa4260ec80d2dc0894e3e7f7591ee9cae27005
Tam Nguyen Ngoc <34363701+nntam2013@users.noreply.github.com>
2021-11-15 15:54:02 +0700

    feat: deploy airdrop (#101)

--- migrations/57_upgrade_xblade.js
+++ migrations/57_upgrade_xblade.js
@@ -5,10 +5,10 @@ const xBlade = artifacts.require("xBlade");
 module.exports = async function (deployer, network, accounts) {
   if (network === "bscmainnet") {
     const xBladeTokenProxy = "0x27a339d9b59b21390d7209b78a839868e319301b";
-    await upgradeProxy(xBladeTokenProxy, xBlade, {
-      deployer,
-    });
+    // await upgradeProxy(xBladeTokenProxy, xBlade, {
+    //   deployer,
+    // });
     const xBladeToken = await xBlade.at(xBladeTokenProxy);
-    await xBladeToken.setAirdropEnabled(true);
+    await xBladeToken.setAirdropEnabled(false);
   }
 };
