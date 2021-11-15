23920f4976b867e949ecf735c6caf76ba9d0a09b
Tam Nguyen <nntam2013@gmail.com>
2021-11-15 22:11:28 +0700

    feat: add migration

---
+++ migrations/58_referral_mint_hero.js
@@ -0,0 +1,21 @@
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
+
+const CWController = artifacts.require("CWController");
+const CryptoWars = artifacts.require("CryptoWars");
+
+module.exports = async function (deployer, network, accounts) {
+  if (network === "bscmainnet") {
+    const cwProxyAddress = "0xAadfa537ecA54d3d7655C4117bBFB83B9bF6035a";
+    await upgradeProxy(cwProxyAddress, CWController, {
+      deployer,
+    });
+    const controller = await CWController.at(cwProxyAddress);
+    await controller.setDiscountRate(7);
+    await controller.setBonusRate(10);
+    const gameProxyAddress = "0x8BA9f0841cFA75d7e2c7a316b048b04c98C95cA4"; //mainnet
+    await upgradeProxy(gameProxyAddress, CryptoWars, {
+      deployer,
+      unsafeAllow: ["external-library-linking"],
+    });
+  }
+};
