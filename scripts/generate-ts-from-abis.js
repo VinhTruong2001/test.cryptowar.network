6784579cc4b9c89de804c84f69fead888151b0d8
Tam Nguyen <nntam2013@gmail.com>
2021-11-20 22:01:12 +0700

    update script

--- scripts/generate-ts-from-abis.js
+++ scripts/generate-ts-from-abis.js
@@ -90,6 +90,7 @@ function transformAbiTypeToOutputTsType(abiType) {
     const m = solidityTypeGrammar.match(abiType);
 
     if(!m.succeeded()) {
+        return
         throw new Error(`Unknown ABI type "${abiType}"`);
     }
 
