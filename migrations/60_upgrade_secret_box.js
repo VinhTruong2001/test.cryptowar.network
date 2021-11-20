3454a2964841427a3011e62773cafdf0d838f7ab
Tam Nguyen <nntam2013@gmail.com>
2021-11-20 22:09:27 +0700

    feat: update migration

--- migrations/60_upgrade_secret_box.js
+++ migrations/60_upgrade_secret_box.js
@@ -14,10 +14,13 @@ module.exports = async function (deployer, network, accounts) {
   }
   if (network === "bscmainnet") {
     const proxyAddress = "0xFc658Da47B952223Fbf2AB0a00dCc609d07a2E32";
-    // await upgradeProxy(proxyAddress, xBlade, {
-    //   deployer,
-    // });
+    await upgradeProxy(proxyAddress, SecretBox, {
+      deployer,
+    });
     const secretBox = await SecretBox.at(proxyAddress);
-    await secretBox.setCwController(true);
+    const cwControllerAddress = "0xAadfa537ecA54d3d7655C4117bBFB83B9bF6035a";
+    await secretBox.setCwController(cwControllerAddress);
+    await secretBox.setCommonBoxPrice(50 * 10 ** 18);
+    await secretBox.setRareBoxPrice(100 * 10 ** 18);
   }
 };
