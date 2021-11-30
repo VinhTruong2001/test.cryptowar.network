fa0d44718388d65a23c018626678316c6bc26f6f
Tam Nguyen <nntam2013@gmail.com>
2021-11-30 23:34:56 +0700

    feat: update migration

--- migrations/61_deploy_blind_box.js
+++ migrations/61_deploy_blind_box.js
@@ -9,21 +9,20 @@ module.exports = async function (deployer, network, accounts) {
   if (network === "bsctestnet") {
     characterAddress = "0x169e12572b318984f0e6ba89230ad2060f7dbb24";
     weaponAddress = "0x2cb15b0a2bd10beaad6105e0b7219d6edc8288a1";
-    blindBox = await deployProxy(BlindBox, [], {
-      deployer,
-    });
   }
   if (network === "bscmainnet") {
-    const proxyAddress = "0xFc658Da47B952223Fbf2AB0a00dCc609d07a2E32";
-    await deployProxy(proxyAddress, SecretBox, {
-      deployer,
-    });
+    characterAddress = "0x169e12572b318984f0e6ba89230ad2060f7dbb24";
+    weaponAddress = "0x2cb15b0a2bd10beaad6105e0b7219d6edc8288a1";
   }
-
-  await upgradeProxy(characterAddress, Characters, {
+  blindBox = await deployProxy(BlindBox, [], {
     deployer,
   });
+
+
   await upgradeProxy(weaponAddress, Weapons, {
+    deployer,
+  });
+  await upgradeProxy(characterAddress, Characters, {
     deployer,
   });
 
