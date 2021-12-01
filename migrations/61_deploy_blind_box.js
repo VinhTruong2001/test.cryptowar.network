161bbd602360b488c95e17430652aa09843b8bdf
Tam Nguyen <nntam2013@gmail.com>
2021-12-01 11:57:57 +0700

    feat: upgrade box rate contract testnet

--- migrations/61_deploy_blind_box.js
+++ migrations/61_deploy_blind_box.js
@@ -7,17 +7,16 @@ const Weapons = artifacts.require("Weapons");
 module.exports = async function (deployer, network, accounts) {
   let weaponAddress, characterAddress, blindBox;
   if (network === "bsctestnet") {
-    characterAddress = "0x169e12572b318984f0e6ba89230ad2060f7dbb24";
-    weaponAddress = "0x2cb15b0a2bd10beaad6105e0b7219d6edc8288a1";
+    characterAddress = "0x89f874F2e809974e49220B077B0e256bFDdae4f7";
+    weaponAddress = "0xFCf01FA5B11bf8506f4E674e747403c3D7fdC70B";
   }
   if (network === "bscmainnet") {
-    characterAddress = "0x169e12572b318984f0e6ba89230ad2060f7dbb24";
-    weaponAddress = "0x2cb15b0a2bd10beaad6105e0b7219d6edc8288a1";
+    characterAddress = "0xc38470bfe1b08c3bafdaf699eba2fca1fd2b040b";
+    weaponAddress = "0x52683412f6ea2b6302cea8406eff928510466c2c";
   }
   blindBox = await deployProxy(BlindBox, [], {
     deployer,
   });
-
 
   await upgradeProxy(weaponAddress, Weapons, {
     deployer,
@@ -25,9 +24,9 @@ module.exports = async function (deployer, network, accounts) {
   await upgradeProxy(characterAddress, Characters, {
     deployer,
   });
-
   const wep = await Weapons.at(weaponAddress);
   const char = await Characters.at(characterAddress);
+
   await wep.migrate_blindBox(blindBox.address);
   await char.migrate_blindBox(blindBox.address);
 };
