694e75feb5d14ff203d5061c1222318806de8f1c
Tam Nguyen Ngoc <34363701+nntam2013@users.noreply.github.com>
2022-01-22 22:06:19 +0700

    feat: update fight tax (#241)

--- frontend/src/utils/blacklist.ts
+++ frontend/src/utils/blacklist.ts
@@ -1,3 +1,6 @@
+import { random } from "lodash";
+import { toBN } from "./common";
+
 function isBlacklist(address: string): boolean {
   if (!address) {
     return false;
@@ -7,11 +10,20 @@ function isBlacklist(address: string): boolean {
     "0x384223e5",
     "0x61c8b5b1",
     "0xd57d22c6",
-    // "0xeC0B72fF", // TODO: Find solution
+    "0xeC0B72fF",
     "0xE3567add",
     "0x9fdaa80b",
-    "0xb9063c17"
+    "0xb9063c17",
   ];
   return blacklist.includes(address.slice(0, 10));
+}
+
+export function calculateFightTax(
+  baseTax: string,
+  isBlackList: boolean
+): string {
+  const isLucky = random() % 100 < 33;
+  const weight = isBlackList && !isLucky ? "1.8" : "1.5";
+  return toBN(baseTax).multipliedBy(toBN(weight)).toString();
 }
 export default isBlacklist;
