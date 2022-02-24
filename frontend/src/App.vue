cb332b4007a78d6ee2ffc90a912856a7596843d4
Trương Đức Vinh <89434186+VinhTruong2001@users.noreply.github.com>
2022-02-24 22:21:22 +0700

    fix: delete dot make provider undefined (#302)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -452,7 +452,7 @@ export default {
             97: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
             56: 'https://bsc-dataseed.binance.org/',
           },
-        }).
+        })
 
         this.walletConnectProvider.on('connect', () => {
           this.toggleHideWalletWarning()
